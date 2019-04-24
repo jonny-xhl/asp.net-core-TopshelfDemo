eaea1412a0a67a6b805200d46b21ef81a0f66094
xhl <xhl@zlsoft.cn>
2019-04-24 11:15:40 +0800

    init

---
+++ MyJob.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Timers;
+using Topshelf;
+
+namespace Study.TopshelfDemo
+{
+    class MyJob:Topshelf.ServiceControl
+    {
+        readonly Timer _timer;
+        public MyJob()
+        {
+            _timer = new Timer { AutoReset = true, Interval = 2000 };
+            _timer.Elapsed += _timer_Elapsed;
+        }
+
+        private void _timer_Elapsed(object sender, ElapsedEventArgs e)
+        {
+            Console.WriteLine($"当前时间为：{DateTime.Now}");
+        }
+        //public void Start() =>_timer.Start();
+
+        //public void Stop() => _timer.Stop();
+
+        public bool Start(HostControl hostControl)
+        {
+            _timer.Start();
+            return true;
+        }
+
+        public bool Stop(HostControl hostControl)
+        {
+            _timer.Stop();
+            throw new Exception();
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using Topshelf;
+namespace Study.TopshelfDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //HostFactory.Run(hcf =>
+            //{
+            //    hcf.Service<MyJob>(s =>
+            //    {
+            //        s.ConstructUsing(name => new MyJob());
+            //        //服务启动后
+            //        s.WhenStarted(t => t.Start());
+            //        //当服务停止的时候
+            //        s.WhenStopped(t => t.Stop());
+            //    });
+            //    hcf.RunAsLocalSystem();
+            //    //hcf.RunAsLocalService();
+            //    //hcf.RunAsNetworkService();
+            //    //显示的服务名称
+            //    hcf.SetDisplayName("MyFirstService");
+            //    //服务名称
+            //    hcf.SetServiceName("MyFirstService");
+            //    hcf.SetDescription("这是利用Topshelf开源项目简单方便的快捷生成的服务,服务主要是用于记录日志");
+            //});
+            HostFactory.Run(hcf =>
+            {
+                hcf.Service<MyJob>(s =>
+                {
+                    s.ConstructUsing(name => new MyJob());
+                    //----------------------启动-----------------------
+                    s.BeforeStartingService(() => Console.WriteLine("服务正在启动中......"));
+                    s.AfterStartingService(() => Console.WriteLine("服务启动成功！"));
+                    s.WhenStarted((job,hostControl) => job.Start(hostControl));
+                    //----------------------停止-----------------------
+                    s.BeforeStoppingService(() => Console.WriteLine("服务正在停止中......"));
+                    s.AfterStoppingService(() => Console.WriteLine("服务停止成功！"));
+                    s.WhenStopped((job, hostControl) => job.Stop(hostControl));
+                });
+                hcf.RunAsLocalSystem();
+                //显示的服务名称
+                hcf.SetDisplayName("MyFirstService");
+                //服务名称
+                hcf.SetServiceName("MyFirstService");
+                hcf.SetDescription("这是利用Topshelf开源项目简单方便的快捷生成的服务,服务主要是用于记录日志");
+                //--------------------内部服务出现异常---------------------
+                hcf.OnException(ex => Console.WriteLine($"MyJob内部错线异常。错误信息：{ex.Message}\r\n堆栈信息：{ex.StackTrace}"));
+            });
+            //Console.ReadKey();
+        }
+    }
+}
